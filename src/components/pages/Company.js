a9bbf32d2c05e928f5a6a42d02404825c84bb5f0
EduPassero <edupassero47@gmail.com>
2023-02-03 00:30:55 -0300

    contato e sobre

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -2,6 +2,10 @@ function Company(){
     return(
         <div>
             <h1>Company</h1>
+            <p>A Costs facilita sua vida permitindo uma vida mais organizada!<br></br>
+            Deseja comprar um carro? Economizar? Assaltar um banco? Hackear a NASA? <br></br>
+            Não importa a tarefa, com o Costs você chega lá!
+            </p>
         </div>
     )
 }
--- src/components/pages/Contato.js
+++ src/components/pages/Contato.js
@@ -2,6 +2,7 @@ function Contato(){
     return(
         <div>
             <h1>Contato</h1>
+            <h2>No momento estamos indisponíveis</h2>
         </div>
     )
 }
